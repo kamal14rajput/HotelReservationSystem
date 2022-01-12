4106082e283c3ac5a735817c0d7522fe74ec3621
kamal14rajput <kamal14rajput@gmail.com>
2022-01-12 09:13:38 +0530

    ADDED: added hotel details

---
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+public class HotelReservationMain  {
+	
+	
+	
+	
+	public static void main( String[] args )
+    {
+        System.out.println(" Welcome to hotel reservation program!" );
+    }
+	
+	
+
+}
