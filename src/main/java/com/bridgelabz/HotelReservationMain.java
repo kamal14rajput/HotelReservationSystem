b02699796d30f1703c6b958107aaf917bb85b094
kamal14rajput <kamal14rajput@gmail.com>
2022-01-23 18:18:51 +0530

    UPDATED: cheapest rate for weekday and weekend

--- src/main/java/com/bridgelabz/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -1,12 +1,21 @@
 package com.bridgelabz;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class HotelReservationMain {
 
 	public static void main(String[] args) {
+
 		System.out.println(" Welcome To Hotel Reservation");
 		IHotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel();
+		hotelReservation.addHotel("Lakewood", 3, 110,90);
+		hotelReservation.addHotel("Bridgewood", 4, 150,50);
+		hotelReservation.addHotel("Ridgewood", 5, 220,150);
 		hotelReservation.printHotelList();
+		LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);    
+		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);    
+		hotelReservation.getCheapestHotel(startDate, endDate);
 	}
 
 }
