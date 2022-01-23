5b9f822896616265e75f9e6e3a8255a12313c862
kamal14rajput <kamal14rajput@gmail.com>
2022-01-23 17:37:48 +0530

    UPDATED: hotel details

--- src/main/java/com/bridgelabz/HotelReservationMain.java
+++ src/main/java/com/bridgelabz/HotelReservationMain.java
@@ -1,13 +1,12 @@
 package com.bridgelabz;
 
+public class HotelReservationMain {
 
-public class HotelReservationMain  {
-	
-	public static void main( String[] args )
-    {
-        System.out.println(" Welcome to hotel reservation program!" );
-    }
-	
-	
+	public static void main(String[] args) {
+		System.out.println(" Welcome To Hotel Reservation");
+		IHotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel();
+		hotelReservation.printHotelList();
+	}
 
 }
