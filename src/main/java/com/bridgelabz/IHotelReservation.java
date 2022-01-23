4c4d51ff708f9df6e64bf7311631ccd4684f2bb9
kamal14rajput <kamal14rajput@gmail.com>
2022-01-23 18:09:10 +0530

    UPDATED: weekday and weekend rates

--- src/main/java/com/bridgelabz/IHotelReservation.java
+++ src/main/java/com/bridgelabz/IHotelReservation.java
@@ -1,9 +1,10 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public interface IHotelReservation {
-	public void addHotel();
-
+	public void addHotel(String hotelName,int rating, double weekDayRate,double weekendRate);
 	public int getHotelListSize();
-
 	public void printHotelList();
+	public ArrayList<HotelDetails> getHotelList();
 }
