2abce924d8036171c882aa55550efa8167f438f1
kamal14rajput <kamal14rajput@gmail.com>
2022-01-23 17:38:26 +0530

    UPDATED: test for hotel details

--- src/test/java/com/bridgelabz/JUnitHotelReservation.java
+++ src/test/java/com/bridgelabz/JUnitHotelReservation.java
@@ -1,5 +1,14 @@
 package com.bridgelabz;
 
-public class JUnitHotelReservation {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class JUnitHotelReservation {
+	@Test
+	public void givenHotelDetails_WhenSizeMatches_ShoulReturnTrue() {
+		IHotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotel();
+		int hotelListSize = hotelReservation.getHotelListSize();
+		Assert.assertEquals(3, hotelListSize);
+	}
 }
