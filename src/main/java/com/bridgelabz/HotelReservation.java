b19115d99f0977e491c521c2754290cb53fe0a94
kamal14rajput <kamal14rajput@gmail.com>
2022-01-23 17:37:32 +0530

    UPDATED: hotel details

--- src/main/java/com/bridgelabz/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -1,15 +1,36 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HotelReservation implements IHotelReservation{
-	ArrayList<HotelDetails> hotelList = new ArrayList<>();
-	public void addHotelDetails() {
-		
-		hotelList.add(new HotelDetails("Lakewood", 110));
-        hotelList.add(new HotelDetails("Bridgewood", 150));
-        hotelList.add(new HotelDetails("Ridgewood", 220));
+	String hotelName;
+	int rating;
+	double regularCustomerRate;
+	Scanner sc = new Scanner(System.in);
+	ArrayList<HotelDetails> hotelList = new ArrayList<HotelDetails>();
+	HotelDetails hotel;
+	public void addHotel() {
+		System.out.println("How many hotels you want to add");
+		int numOfHotels = sc.nextInt();
+		while(numOfHotels > 0) {
+			System.out.println("Enter hotel name:");
+			hotelName = sc.next();
+			System.out.println("Enter hotel rating:");
+			rating = sc.nextInt();
+			System.out.println("Enter regular customer rate:");
+			regularCustomerRate = sc.nextDouble();
+			hotel = new HotelDetails(hotelName,rating,regularCustomerRate);
+			hotelList.add(hotel);
+			numOfHotels--;
+		}
+	}
+	public int getHotelListSize() {
+		return hotelList.size();
 	}
 	
+	public void printHotelList() {
+		System.out.println(hotelList);
+	}
 	
 }
