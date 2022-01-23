96ced898448ee2ff1229f54278ae4fced4ce897b
kamal14rajput <kamal14rajput@gmail.com>
2022-01-23 18:18:59 +0530

    UPDATED: cheapest rate for weekday and weekend

--- src/main/java/com/bridgelabz/IHotelReservation.java
+++ src/main/java/com/bridgelabz/IHotelReservation.java
@@ -1,9 +1,17 @@
 package com.bridgelabz;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public interface IHotelReservation {
-	public void addHotel();
+	public void addHotel(String hotelName, int rating, double weekDayRate, double weekendRate);
 
 	public int getHotelListSize();
 
 	public void printHotelList();
+
+	public ArrayList<HotelDetails> getHotelList();
+
+	public String getCheapestHotel(LocalDate startDate, LocalDate endDate);
+
 }
